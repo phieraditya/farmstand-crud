66557a29f3c7b29e2f4cb03cc44df4ebeba8528b
Devinoraya Radityapalma <devinoraya.radityapalma@gmail.com>
2022-11-08 14:57:17 +0700

    Write products index

--- index.js
+++ index.js
@@ -18,8 +18,10 @@ mongoose
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 
-app.get('/kopi', (req, res) => {
-  res.send('Es Kopi Susu');
+app.get('/products', async (req, res) => {
+  const products = await Product.find({});
+  console.log(products);
+  res.render('products/index', { products });
 });
 
 app.listen(3000, () => {
